ce80945eb497af924bde36ff81e5b8c99035b4e5
Nazaré da Piedade <nazarepiedady@gmail.com>
2020-10-03 10:32:05 +0100

    Create and add `checkForMatch` function
    It was created a function to check the equality between two cards
    if the cards was been equal the card image it's changed to a white
    image and the alert is called with `You found a match` for sucess
    message or `Sorry, try again` to fail message.

--- main.js
+++ main.js
@@ -18,8 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   const $grid = document.querySelector('.grid');
+  const $resultDisplay = document.querySelector('#result');
   var cardsChosen = [];
   var cardsChosenId = [];
+  var cardsWon = [];
 
   // create your board
   function createBoard() {
@@ -33,7 +35,27 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // check for matches
-  function checkForMatch() {}
+  function checkForMatch() {
+    var cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match');
+      cards[optionOneId].setAttribute('src', 'images/white.png');
+      cards[optionTwoId].setAttribute('src', 'images/white.png');
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png');
+      cards[optionTwoId].setAttribute('src', 'images/blank.png');
+      alert('Sorry, try again');
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    $resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length/2) {
+      $resultDisplay.textContent = 'Congratulations! You found them all!';
+    }
+  }
 
   // flip your card
   function flipCard() {
