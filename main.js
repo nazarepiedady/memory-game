e6d0682b6c15cd91b7206b807e20eaa71ca6f643
Nazaré da Piedade <nazarepiedady@gmail.com>
2020-10-03 08:23:01 +0100

    Create the board
    In this point was created a `createBoard` function that will loop
    through over cardArray length to create a number of image element
    equivalent to the number of item in array, and add all them to the
    element with `.grid` class that is in our index.html document.

--- main.js
+++ main.js
@@ -15,4 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
   	{ name: 'milkshake', img: 'images/milkshake.png' },
     { name: 'hotdog', img: 'images/hotdog.png' }
 	];
+
+
+  const $grid = document.querySelector('.grid');
+
+  // create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var $card = document.createElement('img');
+      $card.setAttribute('src', 'images/blank.png');
+      $card.setAttribute('data-id', i);
+      //$card.addEventListener('click', flipcard);
+      $grid.appendChild($card);
+    }
+  }
+
+  createBoard();
 });
